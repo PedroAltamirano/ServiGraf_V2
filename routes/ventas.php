4768e86913f54a32e6da1fe6eb9921da74f6a3c9
PedroAltamirano <pedroaal04@gmail.com>
2020-12-25 13:13:19 -0500

    syntaxis

--- routes/ventas.php
+++ routes/ventas.php
@@ -1,5 +1,8 @@
 <?php
-
-// Route::get('/ventas', '')->name('crm');
-Route::post('/ventas/telefono', 'Ventas\Clientes@telefono')->name('cliente.telefono');
-Route::post('/ventas/cliente', 'Ventas\Clientes@store')->name('contacto.cliente.post');
+Route::namespace('Ventas')
+// ->middleware('hasModRol:80,1')
+->group(function () {
+  // Route::get('/ventas', '')->name('crm');
+  Route::post('cliente/telefono', 'Clientes@telefono')->name('cliente.telefono');
+  Route::post('cliente/store', 'Clientes@store')->name('cliente.store');
+});
