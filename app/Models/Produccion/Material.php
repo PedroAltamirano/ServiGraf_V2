a67cf9bbc00d068df24eaa320a4ea0118135b012
PedroAltamirano <pedroaal04@gmail.com>
2021-02-20 16:18:03 -0500

    material model

--- app/Models/Produccion/Material.php
+++ app/Models/Produccion/Material.php
@@ -6,21 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
-    protected $table = 'materiales';
+  protected $table = 'materiales';
 
-    public $attributes =[
-        'color' => 0, 'uv' => 0, 'plastificado' => 0
-    ];
+  public $attributes =[
+    'color' => 0, 'uv' => 0, 'plastificado' => 0
+  ];
 
-    protected $fillable = [
-        'empresa_id', 'descripcion', 'categoria_id', 'color', 'alto', 'ancho', 'precio', 'uv', 'plastificado'
-    ];
+  protected $fillable = [
+    'empresa_id', 'descripcion', 'categoria_id', 'color', 'alto', 'ancho', 'precio', 'uv', 'plastificado'
+  ];
 
-    protected $hidden = [
-        'created_at', 'updated_at', 
-    ];
+  protected $hidden = [
+    'created_at', 'updated_at',
+  ];
 
-    public function categoria(){
-        return $this->belongsTo('App\Models\Produccion\Categoria');
-    }
+  public function categoria(){
+    return $this->belongsTo('App\Models\Produccion\Categoria');
+  }
 }
