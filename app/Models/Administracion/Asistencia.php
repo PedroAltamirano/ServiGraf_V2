f8beb2582266515f8e7a42c9d3ed4bd9e3e97aa5
PedroAltamirano <pedroaal04@gmail.com>
2021-02-21 21:00:33 -0500

    model

--- app/Models/Administracion/Asistencia.php
+++ app/Models/Administracion/Asistencia.php
@@ -4,7 +4,25 @@ namespace App\Models\Administracion;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\models\Sistema\Nomina;
+
 class Asistencia extends Model
 {
-    //
+  protected $table = 'asistencias';
+
+  protected $fillable = [
+    'empresa_id', 'usuario_id', 'fecha', 'llegada_mañana', 'salida_mañana', 'llegada_tarde', 'salida_tarde', 'total', 'extras'
+  ];
+
+  public $attributes = [
+  ];
+
+  /**
+   * Get the user that owns the Asistencia
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function nomina() {
+      return $this->belongsTo(Nomina::class, 'usuario_id', 'cedula');
+  }
 }
