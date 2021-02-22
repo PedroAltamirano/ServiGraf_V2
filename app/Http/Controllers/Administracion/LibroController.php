52971f02762d102223c5a08c38c5677590c2661e
PedroAltamirano <pedroaal04@gmail.com>
2021-02-21 19:18:58 -0500

    crud libro

--- app/Http/Controllers/Administracion/LibroController.php
+++ app/Http/Controllers/Administracion/LibroController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Administracion;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-use App\Models\Usuarios\Usuario;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Usuarios\Usuario;
 use App\Models\Administracion\Libro_movimientos;
+use App\Models\Administracion\Libro;
 
 use App\Http\Resources\Administracion\LibroResource;
+use App\Http\Requests\Administracion\StoreLibro;
+use App\Http\Requests\Administracion\UpdateLibro;
+use App\Models\Administracion\Banco;
+use App\Models\Administracion\Libro_ref;
 
 class LibroController extends Controller
 {
@@ -22,68 +26,53 @@ class LibroController extends Controller
     public function index()
     {
       $usuarios = Usuario::where('empresa_id', Auth::user()->empresa_id)->where('libro', 1)->get();
-      return view('Administracion.libro', compact('usuarios'));
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+      return view('Administracion.libros', compact('usuarios'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\Administracion\StoreLibro  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreLibro $request)
     {
-        //
-    }
+      $validated = $request->validated();
+      $validated['empresa_id'] = Auth::user()->empresa_id;
+      $libro = Libro::create($validated);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+      $data = [
+      'type'=>'success',
+      'title'=>'Acción completada',
+      'message'=>'El libro se ha creado con éxito'
+    ];
+      return redirect()->back()->with(['actionStatus' => json_encode($data)]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  App\Http\Requests\Administracion\UpdateLibro  $request
+     * @param  App\Models\Administracion\Libro  $libro
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateLibro $request, Libro $libro)
     {
-        //
+      $validated = $request->validated();
+      $libro->update($validated);
+
+      $data = [
+      'type'=>'success',
+      'title'=>'Acción completada',
+      'message'=>'El libro se ha modificado con éxito'
+    ];
+      return redirect()->back()->with(['actionStatus' => json_encode($data)]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int  App\Models\Administracion\  $entrada
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -91,10 +80,18 @@ class LibroController extends Controller
         //
     }
 
+    public function referencias_bancos()
+    {
+      $referencias = Libro_ref::where('empresa_id', Auth::user()->empresa_id)->get();
+      $bancos = Banco::where('empresa_id', Auth::user()->empresa_id)->get();
+      return view('Administracion.referencias-bancos', compact('referencias', 'bancos'));
+    }
+
     public function api_libros(Request $request) {
       $res = Usuario::find($request->usuario)->libros;
       return response()->json($res, 200);
     }
+
     public function api_info(Request $request) {
       $movimientos = Libro_movimientos::where('usuario_id', $request->usuario)->where('libro_id', $request->libro)->whereBetween('fecha', [$request->fechaini, $request->fechafin])->get();
 
--- app/Models/Administracion/Libro.php
+++ app/Models/Administracion/Libro.php
@@ -3,8 +3,16 @@
 namespace App\Models\Administracion;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Libro extends Model
 {
-    //
+  protected $table = 'libros';
+
+  protected $fillable = [
+    'empresa_id', 'usuario_id', 'libro'
+  ];
+
+  public $attributes = [
+  ];
 }
