de7b8b577616564e5f7d8c176727ee1def1f8b18
PedroAltamirano <pedroaal04@gmail.com>
2021-02-23 20:54:11 -0500

    fix

--- app/View/Components/modalPedido.php
+++ app/View/Components/modalPedido.php
@@ -9,24 +9,25 @@ class modalPedido extends Component
 {
   public $pedido;
   public $method;
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct($id = null)
-    {
-      $this->pedido = $id ? Pedido::find($id) : new Pedido;
-      $this->method = 'PUT';
-    }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|string
-     */
-    public function render()
-    {
-        return view('components.modal-pedido');
-    }
+  /**
+   * Create a new component instance.
+   *
+   * @return void
+   */
+  public function __construct($id = null)
+  {
+    $this->pedido = $id ? Pedido::find($id) : new Pedido;
+    $this->method = 'PUT';
+  }
+
+  /**
+   * Get the view / contents that represent the component.
+   *
+   * @return \Illuminate\Contracts\View\View|string
+   */
+  public function render()
+  {
+      return view('components.modal-pedido');
+  }
 }
