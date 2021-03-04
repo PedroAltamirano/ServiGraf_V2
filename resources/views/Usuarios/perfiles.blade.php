fa1e412684fb25117e9c3ad9dd457091ad93944d
PedroAltamirano <pedroaal04@gmail.com>
2021-03-03 20:59:54 -0500

    datatable

--- resources/views/Usuarios/perfiles.blade.php
+++ resources/views/Usuarios/perfiles.blade.php
@@ -39,34 +39,34 @@
 
 @section('scripts')
 <script>
-  $(document).ready(function() {
-    $('#table').DataTable({
-      "paging":   true,
-      "ordering": true,
-      "info":     false,
-      "responsive": true,
-      "ajax": {
-        "url": "{{url('/perfiles/get')}}",
-        "method": 'get',
-        "error": function(reason) {
-          alert('Ha ocurrido un error al cargar los datos!');
-        }
-      },
-      "columns": [
-        {"name":"nombre", "data":"nombre"},
-        {"name":"descripcion", "data":"descripcion"},
-        {"name":"crud", "data":"id",
-          "render": function ( data, type, full, meta ) {
-            return "<a class='fa fa-edit' href='perfil/modificar/"+data+"'></a>";
-          }
-        }
-      ],
-      "columnDefs": [
-      ],
-      "rowCallback": function(row, data, index){
-        (data.status == 1) ? $('td:eq(-1)', row).children('a').addClass('text-success') : $('td:eq(-1)', row).children('a').addClass('text-danger');
-      },
-    });
-  });
+  // $(document).ready(function() {
+  //   $('#table').DataTable({
+  //     "paging":   true,
+  //     "ordering": true,
+  //     "info":     false,
+  //     "responsive": true,
+  //     "ajax": {
+  //       "url": "{{url('/perfiles/get')}}",
+  //       "method": 'get',
+  //       "error": function(reason) {
+  //         alert('Ha ocurrido un error al cargar los datos!');
+  //       }
+  //     },
+  //     "columns": [
+  //       {"name":"nombre", "data":"nombre"},
+  //       {"name":"descripcion", "data":"descripcion", "sortable":"false"},
+  //       {"name":"crud", "data":"id", "sortable":"false",
+  //         "render": function ( data, type, full, meta ) {
+  //           return "<a class='fa fa-edit' href='perfil/modificar/"+data+"'></a>";
+  //         }
+  //       }
+  //     ],
+  //     "columnDefs": [
+  //     ],
+  //     "rowCallback": function(row, data, index){
+  //       (data.status == 1) ? $('td:eq(-1)', row).children('a').addClass('text-success') : $('td:eq(-1)', row).children('a').addClass('text-danger');
+  //     },
+  //   });
+  // });
 </script>
 @endsection
