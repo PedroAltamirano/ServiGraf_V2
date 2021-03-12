85c09ac2fd9d19d647dbf8c968a7b3cd8b99d283
PedroAltamirano <pedroaal04@gmail.com>
2021-03-11 21:14:36 -0500

    dotacion init

---
+++ app/Http/Requests/Administracion/StoreDotacion.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Administracion;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDotacion extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'dotacion' => ['required', 'string'],
+          'status' => ['required', 'string']
+        ];
+    }
+}
