c6106665446c548dd39afe745443410c2e8e3b74
PedroAltamirano <pedroaal04@gmail.com>
2021-04-16 11:33:54 -0500

    model

--- app/Models/Sistema/Horario.php
+++ app/Models/Sistema/Horario.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Horario extends Model
 {
-    protected $table = 'horarios';
-    protected $primaryKey = 'id';
+  protected $table = 'horarios';
+  protected $primaryKey = 'id';
 
-    protected $attributes = [
-        'espera' => 10, 'gracia' => 5
-    ];
+  protected $attributes = [
+    'espera' => 10, 'gracia' => 5
+  ];
 
-    protected $fillable = [
-        'empresa_id', 'nombre', 'llegada_ma', 'salida_ma', 'llegada_ta', 'salida_ta', 'espera', 'gracia',
-    ];
+  protected $fillable = [
+    'empresa_id', 'nombre', 'llegada_ma', 'salida_ma', 'llegada_ta', 'salida_ta', 'espera', 'gracia',
+  ];
 }
