2105483f951ff28819d96988c9d1d713f9452ea0
PedroAltamirano <pedroaal04@gmail.com>
2021-04-16 11:33:11 -0500

    centro de costos

--- app/Http/Controllers/Sistema/Empresa.php
+++ app/Http/Controllers/Sistema/Empresa.php
@@ -10,12 +10,14 @@ use App\Models\Sistema\DatosEmpresa;
 use App\Models\Sistema\Fact_empr;
 use App\Http\Requests\Sistema\StoreEmpresa;
 use App\Http\Requests\Sistema\UpdateEmpresa;
+use App\Models\Sistema\CentroCostos;
 
 class Empresa extends Controller
 {
     public function show(){
-        $empresa = Auth::user()->empresa->datos ?? new DatosEmpresa;
-        return view('Sistema.empresa', compact('empresa'));
+      $empresa = Auth::user()->empresa->datos ?? new DatosEmpresa;
+      $ccostos = CentroCostos::where('empresa_id', Auth::user()->empresa_id)->get();
+      return view('Sistema.empresa', compact('empresa', 'ccostos'));
     }
 
     public function store(StoreEmpresa $request){
