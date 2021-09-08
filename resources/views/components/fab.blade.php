92e013680b898570b5caba181c72799f0add2643
PedroAltamirano <pedroaal04@gmail.com>
2021-09-08 17:06:31 -0500

    added tooltips

--- resources/views/components/fab.blade.php
+++ resources/views/components/fab.blade.php
@@ -4,17 +4,17 @@
   </button>
   <ul class="dropdown-menu dropdown-menu-right">
     <li>
-        <a href="{{route('entrada.create')}}" class="btn btn-primary bg-primary-80 btn-lg mb-2">
+        <a href="{{route('entrada.create')}}" class="btn btn-primary bg-primary-80 btn-lg mb-2" data-toggle="tooltip" data-placement="left" title="Nueva entrada">
             <i class="fa fa-book" aria-hidden="true"></i>
         </a>
     </li>
     <li>
-        <a href="{{route('factura.create')}}" class="btn btn-primary bg-primary-80 btn-lg mb-2">
+        <a href="{{route('factura.create')}}" class="btn btn-primary bg-primary-80 btn-lg mb-2"  data-toggle="tooltip" data-placement="left" title="Nueva factura">
             <i class="fas fa-chart-line"></i>
         </a>
     </li>
     <li>
-        <a href="{{route('pedido.create')}}" class="btn btn-primary bg-primary-80 btn-lg">
+        <a href="{{route('pedido.create')}}" class="btn btn-primary bg-primary-80 btn-lg" data-toggle="tooltip" data-placement="left" title="Nuevo pedido">
             <i class="fa fa-cog" aria-hidden="true"></i>
         </a>
     </li>
