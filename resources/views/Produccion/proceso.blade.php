f762d298163768ccd3d26ef5073f2f0c40115461
PedroAltamirano <pedroaal04@gmail.com>
2021-09-26 11:34:20 -0500

    fix recursive options

--- resources/views/Produccion/proceso.blade.php
+++ resources/views/Produccion/proceso.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('desktop-content')
-<x-path
-  :items="[
+<x-path :items="[
     [
       'text' => 'Procesos',
       'current' => false,
@@ -13,15 +12,11 @@
       'current' => true,
       'href' => '#',
     ]
-  ]"
-/>
+  ]" />
 
-<x-blue-board
-  :title=$text
-  :foot="[
+<x-blue-board :title=$text :foot="[
     ['text'=>$action, 'href'=>'#', 'id'=>'formSubmit', 'tipo'=>'link'],
-  ]"
->
+  ]">
   <form action="{{ $path }}" method="POST" id="form">
     @csrf
     @method($method)
@@ -30,7 +25,8 @@
         <label for="area_id">Área</label>
         <select name="area_id" id="area_id" class="form-control form-control-sm @error('area_id') is-invalid @enderror">
           @foreach ($areas as $item)
-          <option value="{{ $item->id }}" {{ old('area_id', $proceso->area_id) == $item->id ? 'selected' : '' }}>{{ $item->area }}</option>
+          <option value="{{ $item->id }}" {{ old('area_id', $proceso->area_id) == $item->id ? 'selected' : '' }}>
+            {{ $item->area }}</option>
           @endforeach
         </select>
       </div>
@@ -39,11 +35,15 @@
 
       <div class="form-group col-12 col-md-4">
         <label for="proceso">Proceso</label>
-        <input type="text" name="proceso" id="proceso" class="form-control form-control-sm @error('proceso') is-invalid @enderror" value="{{ old('proceso', $proceso->proceso) }}">
+        <input type="text" name="proceso" id="proceso"
+          class="form-control form-control-sm @error('proceso') is-invalid @enderror"
+          value="{{ old('proceso', $proceso->proceso) }}">
       </div>
       <div class="form-group col-6 col-md-4">
         <label for="meta">Meta $</label>
-        <input type="number" step="0.01" min="0" name="meta" id="meta" class="form-control form-control-sm fixFloat @error('meta') is-invalid @enderror" value="{{ old('meta', $proceso->meta) }}">
+        <input type="number" step="0.01" min="0" name="meta" id="meta"
+          class="form-control form-control-sm fixFloat @error('meta') is-invalid @enderror"
+          value="{{ old('meta', $proceso->meta) }}">
       </div>
       <div class="form-group col-6 col-md-4">
         <label for="tipo">Tipo de proceso</label>
@@ -54,16 +54,22 @@
       </div>
       <div class="form-group col-6 col-md-2">
         <label for="tmaquina">T x Máquina</label>
-        <input type="time" min="0" name="tmaquina" id="tmaquina" class="form-control form-control-sm @error('tmaquina') is-invalid @enderror" value="{{ old('tmaquina', $proceso->tmaquina) }}" disabled>
+        <input type="time" min="0" name="tmaquina" id="tmaquina"
+          class="form-control form-control-sm @error('tmaquina') is-invalid @enderror"
+          value="{{ old('tmaquina', $proceso->tmaquina) }}" disabled>
       </div>
       <div class="form-group col-6 col-md-2">
         <label for="toperador">T x operador</label>
-        <input type="time" min="0" name="toperador" id="toperador" class="form-control form-control-sm @error('toperador') is-invalid @enderror" value="{{ old('toperador', $proceso->toperador) }}" disabled>
+        <input type="time" min="0" name="toperador" id="toperador"
+          class="form-control form-control-sm @error('toperador') is-invalid @enderror"
+          value="{{ old('toperador', $proceso->toperador) }}" disabled>
       </div>
       <div class="form-group col-3 col-md-2">
-        <label for="seguimiento">seguimiento</label>
+        <label for="seguimiento">Seguimiento</label>
         <div class="custom-control custom-switch d-flex justify-content-center" name="seguimientoDiv">
-          <input type="checkbox" class="custom-control-input @error('seguimiento') is-invalid @enderror" id="seguimiento" name="seguimiento" {{ old('seguimiento', $proceso->seguimiento) == '1' ? 'checked':'' }} value='1'>
+          <input type="checkbox" class="custom-control-input @error('seguimiento') is-invalid @enderror"
+            id="seguimiento" name="seguimiento" {{ old('seguimiento', $proceso->seguimiento) == '1' ? 'checked':'' }}
+            value='1'>
           <label class="custom-control-label" for="seguimiento"></label>
         </div>
       </div>
--- resources/views/components/recusive-options.blade.php
+++ resources/views/components/recusive-options.blade.php
@@ -1,11 +1,14 @@
 @foreach ($list as $item)
-  {{-- @if ($item->childs->count()) --}}
-  @if ($item->children->count())
-    @if ($with_parent)
-    <option value='' {{ $old == $item->id ? 'selected' : '' }}>{{ $parent.$item->attributes[$column] }}</option>
-    @endif
-    @include('components.recusive-options', ['parent' => $parent.$item->attributes[$column].' / ', 'column' => $column, 'list' => $item->children, 'old' => $old, 'with_parent' => $with_parent])
-  @else
-    <option value='{{ $item->id }}' {{ $old == $item->id ? 'selected' : '' }}>{{ $parent.$item->attributes[$column] }}</option>
-  @endif
+{{-- @if ($item->childs->count()) --}}
+@if ($item->children->count())
+@if ($with_parent)
+<option value='{{ $item->id }}' {{ $old == $item->id ? 'selected' : '' }}>{{ $parent.$item->attributes[$column] }}
+</option>
+@endif
+@include('components.recusive-options', ['parent' => $parent.$item->attributes[$column].' / ', 'column' => $column,
+'list' => $item->children, 'old' => $old, 'with_parent' => $with_parent])
+@else
+<option value='{{ $item->id }}' {{ $old == $item->id ? 'selected' : '' }}>{{ $parent.$item->attributes[$column] }}
+</option>
+@endif
 @endforeach
