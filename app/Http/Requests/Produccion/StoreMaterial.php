654267adb7ad9c6b101fad3c4eb4f3e435fe6a09
PedroAltamirano <pedroaal04@gmail.com>
2021-10-02 15:29:02 -0500

    decimal to 8, 2

--- app/Http/Requests/Produccion/StoreMaterial.php
+++ app/Http/Requests/Produccion/StoreMaterial.php
@@ -6,32 +6,32 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreMaterial extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'descripcion' => ['required', 'string'],
-            'categoria_id' => ['required', 'numeric', 'exists:categorias,id'],
-            'color' => ['nullable', 'boolean'],
-            'alto' => ['required', 'numeric', 'max:130.00'],
-            'ancho' => ['required', 'numeric', 'max:130.00'],
-            'precio' => ['required', 'numeric', 'max:99.99'],
-            'uv' => ['nullable', 'boolean'],
-            'plastificado' => ['nullable', 'boolean']
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'descripcion' => ['required', 'string'],
+      'categoria_id' => ['required', 'numeric', 'exists:categorias,id'],
+      'color' => ['nullable', 'boolean'],
+      'alto' => ['required', 'numeric', 'max:999999.99'],
+      'ancho' => ['required', 'numeric', 'max:999999.99'],
+      'precio' => ['required', 'numeric', 'max:99.99'],
+      'uv' => ['nullable', 'boolean'],
+      'plastificado' => ['nullable', 'boolean']
+    ];
+  }
 }
