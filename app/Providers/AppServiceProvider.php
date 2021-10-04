ef2cbbdcb0a875b4c86cf313ea148c82e4a0e97b
PedroAltamirano <pedroaal04@gmail.com>
2021-10-03 20:02:48 -0500

    fruitcake cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,26 +5,31 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register(){
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-        Paginator::useBootstrap();
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    if (env('APP_ENV') !== 'local') {
+      URL::forceScheme('https');
     }
+    Schema::defaultStringLength(191);
+    Paginator::useBootstrap();
+  }
 }
