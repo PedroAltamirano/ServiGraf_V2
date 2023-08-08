4bd81e28b532bfefcbc4db3b21e11b4dbae315d4
PedroAltamirano <pedroaal04@gmail.com>
2023-08-08 18:16:51 -0500

    ifx

--- app/Models/Usuarios/UsuarioProceso.php
+++ app/Models/Usuarios/UsuarioProceso.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UsuarioProceso extends Model
 {
-  use SoftDeletes;
+  // use SoftDeletes;
 
   protected $table = 'usuario_proceso';
   protected $fillable = [
